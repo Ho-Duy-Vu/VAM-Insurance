5c14c6c3113db9352f06ecae5820a3db67537266
Hồ Duy Vũ <duyvu11092004@gmail.com>
2025-11-13 16:41:34 +0700

    Fix module resolution and export issues for Vercel build

--- Frontend/src/pages/insurance/index.ts
+++ Frontend/src/pages/insurance/index.ts
@@ -1,9 +1,9 @@
 // Insurance Related Pages
-export { ProductsPage } from './ProductsPage'
+export { default as ProductsPage } from './ProductsPage'
 export { PackageDetailPage } from './PackageDetailPage'
-export { NaturalDisasterPackageDetailPage } from './NaturalDisasterPackageDetailPage'
+export { default as NaturalDisasterPackageDetailPage } from './NaturalDisasterPackageDetailPage'
 export { InsuranceApplicationFormPage } from './InsuranceApplicationFormPage'
-export { ImprovedInsuranceApplicationPage } from './ImprovedInsuranceApplicationPage'
-export { NaturalDisasterApplicationPage } from './NaturalDisasterApplicationPage'
+export { default as ImprovedInsuranceApplicationPage } from './ImprovedInsuranceApplicationPage'
+export { default as NaturalDisasterApplicationPage } from './NaturalDisasterApplicationPage'
 export { PaymentPage } from './PaymentPage'
 export { SuccessPage } from './SuccessPage'
